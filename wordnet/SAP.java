72ffa8d4d2acc48bddfdaab4ffcf97798ffb2a2f
Ákos Németh <nemeth.akos@gmail.com>
2019-01-07 23:43:50 +0100

    reformat

--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -22,25 +22,6 @@ public class SAP {
         return getLowestCommonAncestor(Arrays.asList(v), Arrays.asList(w)).distance;
     }
 
-    private void checkIterableIfContainsValidVertexIds(Iterable<Integer> iterable) {
-        if (iterable == null) {
-            throw new IllegalArgumentException("null element in Iterator!");
-        }
-        for (Integer i : iterable) {
-            if (i == null) {
-                throw new IllegalArgumentException("Element in iterator is null!");
-            }
-            if (i < 0 || i >= this.digraph.V()) {
-                throw new IllegalArgumentException(String.format("Node index: %d out of range [0,%d]", i, this.digraph.V()));
-            }
-        }
-    }
-
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w) {
-        return getLowestCommonAncestor(Arrays.asList(v), Arrays.asList(w)).commonAncestorId;
-    }
-
     private LowestCommonAncestor getLowestCommonAncestor(Iterable<Integer> v, Iterable<Integer> w) {
         checkIterableIfContainsValidVertexIds(v);
         checkIterableIfContainsValidVertexIds(w);
@@ -64,6 +45,25 @@ public class SAP {
 
         int distance = commonAncestorId == -1 ? -1 : minDistance;
         return new LowestCommonAncestor(distance, commonAncestorId);
+    }
+
+    private void checkIterableIfContainsValidVertexIds(Iterable<Integer> iterable) {
+        if (iterable == null) {
+            throw new IllegalArgumentException("null element in Iterator!");
+        }
+        for (Integer i : iterable) {
+            if (i == null) {
+                throw new IllegalArgumentException("Element in iterator is null!");
+            }
+            if (i < 0 || i >= this.digraph.V()) {
+                throw new IllegalArgumentException(String.format("Node index: %d out of range [0,%d]", i, this.digraph.V()));
+            }
+        }
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        return getLowestCommonAncestor(Arrays.asList(v), Arrays.asList(w)).commonAncestorId;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
